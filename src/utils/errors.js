f32473ba5dde4aec9a468337479163e148f98527
kesienacruz <105823514+kesienacruz@users.noreply.github.com>
2025-10-26 18:05:48 +0100

    Delete src directory

--- src/utils/errors.js
+++
@@ -1,7 +0,0 @@
-export function makeError(code, message, details = {}) {
-  return { code, message, details };
-}
-
-export const isAuthError = (err) => err?.code === "AUTH_ERROR";
-export const isValidationError = (err) => err?.code === "VALIDATION_ERROR";
-export const isNetworkError = (err) => err?.code === "NETWORK_ERROR";
